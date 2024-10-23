9e4ba647549dcadfd271bb97064dd561a60b111f
Varrowyn <ritzchy04@gmail.com>
2024-10-24 02:08:19 +0800

    syntactic broadcast callbacks

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,8 +5,12 @@ class Quote < ApplicationRecord
   #default target is model_name.plural  -> target: "quotes"
   #default partial is calling to_partial_path on an instance of the model -> partial: "quotes/quote"
   #The locals default value is equal to { model_name.element.to_sym => self } -> locals { quote: self }
-  after_create_commit -> { broadcast_prepend_later_to "quotes" } # asynchronous
-  after_update_commit -> { broadcast_replace_later_to "quotes" } # aysnchronous
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" } # asynchronous
+  # after_update_commit -> { broadcast_replace_later_to "quotes" } # aysnchronous
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+ 
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 
 end
