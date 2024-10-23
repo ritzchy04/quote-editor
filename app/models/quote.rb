84e6304d429de941a895c69f98b877a23f069315
Varrowyn <ritzchy04@gmail.com>
2024-10-24 00:43:18 +0800

    Turbo Streams conventions and syntactic sugar for after_create_commit callback

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,8 +2,9 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc)}
 
-  after_create_commit -> { broadcast_prepend_to "quotes", 
-                           partial: "quotes/quote", 
-                           locals: { quote: self }, 
-                           target: "quotes" }
+  #default target is model_name.plural  -> target: "quotes"
+  #default partial is calling to_partial_path on an instance of the model -> partial: "quotes/quote"
+  #The locals default value is equal to { model_name.element.to_sym => self } -> locals { quote: self }
+  after_create_commit -> { broadcast_prepend_to "quotes"}
+  
 end
