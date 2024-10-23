16686c84ec5f856346bbe8a90d6b05452d0fe431
Varrowyn <ritzchy04@gmail.com>
2024-10-24 02:06:08 +0800

    asynchronous broadcasting

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,6 +5,8 @@ class Quote < ApplicationRecord
   #default target is model_name.plural  -> target: "quotes"
   #default partial is calling to_partial_path on an instance of the model -> partial: "quotes/quote"
   #The locals default value is equal to { model_name.element.to_sym => self } -> locals { quote: self }
-  after_create_commit -> { broadcast_prepend_to "quotes"}
-  
+  after_create_commit -> { broadcast_prepend_later_to "quotes" } # asynchronous
+  after_update_commit -> { broadcast_replace_later_to "quotes" } # aysnchronous
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
+
 end
